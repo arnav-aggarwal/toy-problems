d381f7a80352dd182ed0f9721c4f1ee3238c562e
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-11-21 23:40:33 -0800

    Restart rotate matrix

--- ctci/1/rotate_matrix.js
+++ ctci/1/rotate_matrix.js
@@ -7,39 +7,44 @@
  */
 
 function rotateMatrix(mat) {
-  //go one square at a time
-  for(let i = 0; i < Math.floor(mat.length / 2); i++) {
-    //iterate over the row
-    // console.log(i);
 
-    const firstIndex = i + 1;
-    const lastIndex = mat.length - i;
-    for(let j = firstIndex; j < lastIndex; j++) {
-      // const thisIndex = 
-      console.log('Indices: ', firstIndex, lastIndex, j);
-      // console.log(j);
-      //move everything in place:
-      //temp1 = top[end]
-      let temp1 = mat[firstIndex + j][lastIndex];
-      console.log(temp1);
-      //right[j] = top[j]
-      mat[firstIndex + j][lastIndex] = mat[firstIndex][firstIndex + j];
-      //temp2 = bottom[end - j]
-      let temp2 = mat[lastIndex][lastIndex - j];
-      console.log(temp2);
 
-      //bottom right = temp1
-      mat[lastIndex][lastIndex - j] = temp1;
-      //temp1 = bottom[j]
-      temp1 = mat[lastIndex - j][j];
-      console.log(temp1);
 
-      //bottom[j] = temp2
-      mat[lastIndex - j][j] = temp2;
-      //top[j] = temp1
-      mat[firstIndex][firstIndex + j] = temp1;
-    }
-  }
+
+  
+  // //go one square at a time
+  // for(let i = 0; i < Math.floor(mat.length / 2); i++) {
+  //   //iterate over the row
+  //   // console.log(i);
+
+  //   const firstIndex = i + 1;
+  //   const lastIndex = mat.length - i - 1;
+  //   for(let j = firstIndex; j <= lastIndex; j++) {
+  //     // const thisIndex = 
+  //     console.log('Indices: ', firstIndex, lastIndex, j);
+  //     // console.log(j);
+  //     //move everything in place:
+  //     //temp1 = top[end]
+  //     let temp1 = mat[j][lastIndex - i];
+  //     console.log(temp1);
+  //     //right[j] = top[j]
+  //     mat[j][lastIndex] = mat[i][j];
+  //     //temp2 = bottom[end - j]
+  //     let temp2 = mat[lastIndex - i][lastIndex - j];
+  //     console.log(temp2);
+
+  //     //bottom right = temp1
+  //     mat[lastIndex - i][lastIndex - j] = temp1;
+  //     //temp1 = bottom[j]
+  //     temp1 = mat[lastIndex - j][i];
+  //     console.log(temp1);
+
+  //     //bottom[j] = temp2
+  //     mat[lastIndex - j][j] = temp2;
+  //     //top[j] = temp1
+  //     mat[firstIndex][firstIndex + j] = temp1;
+  //   }
+  // }
 
   return mat;
 }
