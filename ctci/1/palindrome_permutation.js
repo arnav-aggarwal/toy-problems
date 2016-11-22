9ed38b78463dae1820f5951f316289aed27be978
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-11-21 19:31:04 -0800

    Fix: Debug ctci 1 Palindrome Permutation

--- ctci/1/palindrome_permutation.js
+++ ctci/1/palindrome_permutation.js
@@ -12,13 +12,15 @@ function palindromePermutation(str) {
   const obj = {};
   for(let i = 0; i < str.length; i++) {
     const thisChar = str.charAt(i).toLowerCase();
+    if(!thisChar.match(/[a-z]/)) {
+      continue;
+    }
+
     obj[thisChar] = obj[thisChar] + 1 || 1;
   }
-  
-  delete obj[' '];
-  
+    
   let oddCount = 0;
-  const values = Object.values(obj);
+  const values = Object.keys(obj).map(key => obj[key]);
   
   for(let i = 0; i < values.length; i++) {
     if(values[i] % 2) {
--- test/ctci_test.js
+++ test/ctci_test.js
@@ -27,7 +27,7 @@ describe('Is Unique (Cracking the Coding Interview 1.1)', function() {
   runQueueTests(isUniqueSimple, 'Implementation with no extra data structures should return true and false correctly');
 });
 
-describe('Check Permutation (Cracking the Coding Interview 1.1)', function() {
+describe('Check Permutation (Cracking the Coding Interview 1.2)', function() {
   const checkPermutation = require('../ctci/1/check_permutation.js');
   it('Should return whether one string is a permutation of another', function() {
     expect(checkPermutation('', '')).to.be.true;
@@ -45,11 +45,26 @@ describe('Check Permutation (Cracking the Coding Interview 1.1)', function() {
   });
 });
 
-describe('URLify (Cracking the Coding Interview 1.1)', function() {
+describe('URLify (Cracking the Coding Interview 1.3)', function() {
   const URLify = require('../ctci/1/URLify.js');
   it('Should replace spaces with "%20"', function() {
     expect(URLify('')).to.equal('');
     expect(URLify('Hi there  ')).to.equal('Hi%20there');
     expect(URLify('Hi there cutie    ')).to.equal('Hi%20there%20cutie');
+  });
+});
+
+describe('Palindrome Permutation (Cracking the Coding Interview 1.4)', function() {
+  const pal = require('../ctci/1/palindrome_permutation.js');
+  it('Should determine if a string is a permutation of a palindrome', function() {
+    expect(pal('')).to.be.false;
+    expect(pal('a')).to.be.true;
+    expect(pal('ab')).to.be.false;
+    expect(pal('abcab')).to.be.true;
+    expect(pal('zaa  zbbzcc zddz')).to.be.true;
+    expect(pal('zaa  zbbzcc zdddz')).to.be.false;
+    expect(pal(`[]%&^*(*asdfqrt yuiopdghjkl;wtu
+      yiop[dbnvmm,.(&^%$))])[]%&^*(*a sdfqrtyui
+      opdghjkl;wtuyiop[db nvmm,.(&^% $))])`)).to.be.true;
   });
 });
